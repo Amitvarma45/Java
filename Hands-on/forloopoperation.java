b3926e36127b0639519745e6ba9b386ee4fb5d31
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-11 12:16:40 +0530

    Add files via upload

---
+++ Hands-on/forloopoperation.java
@@ -0,0 +1,16 @@
+package Corejava;
+
+public class forloopoperation {
+	
+	public static void main(String[] args)
+	{
+		int a = 2,i,mul;
+		
+		for(i=1;i<=10;i++) {
+			mul=a*i;
+			System.out.println(mul);
+		}
+		
+	}
+
+}

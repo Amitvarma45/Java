9bb060561af41ff1b70d94c798a16a5a347cf073
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-11 10:33:14 +0530

    Add files via upload

---
+++ Hands-on/arithmaticop.java
@@ -0,0 +1,23 @@
+package Corejava;
+
+public class arithmaticop {
+	public static void main(String[] args)
+	{
+		int a= 10;
+		int b= 20;
+		int c;
+		c=a+b;
+		System.out.println(a+"+"+b+"="+c);
+		c=a-b;
+		System.out.println(a+"-"+b+"="+c);
+		c=a*b;
+		System.out.println(a+"*"+b+"="+c);
+		c=a/b;
+		System.out.println(a+"/"+b+"="+c);
+		c=a%b;
+		System.out.println(a+"%"+b+"="+c);
+		
+	}
+	
+
+}

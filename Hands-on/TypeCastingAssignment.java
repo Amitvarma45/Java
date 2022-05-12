0307f1263bf7cd4f86dfb692fe58dad1162ae2de
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-12 12:50:57 +0530

    Add files via upload

---
+++ Hands-on/TypeCastingAssignment.java
@@ -0,0 +1,26 @@
+package Corejava;
+
+public class TypeCastingAssignment {
+	public static void main(String[] args) {
+		
+		
+		//Implicit int to double
+		int i = 36;
+		
+		double d = i;
+		
+		System.out.println("Int value is "+i);
+		System.out.println("Double value is "+d);
+		
+		//Explicit double to int
+		double db = 39.78;
+		
+		int it = (int)db;
+		
+		System.out.println("Double value is "+db);
+		System.out.println("Integer value is "+it);
+		
+		
+	}
+
+}

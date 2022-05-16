b75ae0553d3d1d89006d0f9dffeb2dc0410ea464
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-16 13:43:41 +0530

    Add files via upload

---
+++ Hands-on/encapsulateassign.java
@@ -0,0 +1,19 @@
+package Corejava;
+
+public class encapsulateassign {
+	
+	public static void main (String[] args) {
+	
+	encapsulationassign e = new encapsulationassign();
+	
+	e.setId(2001);
+	e.setName("Amit");
+	e.setAddress("Rabale");
+	
+	System.out.println("Id : "+e.getId());
+	System.out.println("Name : "+e.getName());
+	System.out.println("Address : "+e.getAddress());
+	}
+
+}
+

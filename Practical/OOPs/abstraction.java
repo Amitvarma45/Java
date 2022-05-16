49e82971436d41106fbd772b8731af28d5a44815
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-16 13:42:28 +0530

    Add files via upload

---
+++ Practical/OOPs/abstraction.java
@@ -0,0 +1,15 @@
+package Corejava;
+
+//creating abstract class
+abstract class abstraction {
+	
+	//creating variables
+	public abstract int multiply (int n1, int n2);
+	public abstract int multiplyone (int n1, int n2, int n3);
+	
+	
+	
+	
+
+}
+

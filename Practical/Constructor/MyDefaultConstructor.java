b28c45bc3c5c6a354df42c2c104bb41d986d4a1b
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-19 11:53:44 +0530

    Add files via upload

---
+++ Practical/Constructor/MyDefaultConstructor.java
@@ -0,0 +1,15 @@
+package Corejava;
+
+class MyDefaultConstructor {
+
+	MyDefaultConstructor(){
+		System.out.println("This is Default Constuctor/No arguments Constructor");
+	}
+	
+	public static void main(String[] args) {
+		MyDefaultConstructor mdc= new MyDefaultConstructor();
+		
+
+	}
+
+}

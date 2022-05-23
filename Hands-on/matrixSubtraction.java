bc66d68c5669366597ade2bd9c59915ec6e1a38c
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-23 12:19:12 +0530

    Add files via upload

---
+++ Hands-on/matrixSubtraction.java
@@ -0,0 +1,43 @@
+package Corejava;
+
+import java.util.Scanner; 
+public class matrixSubtraction { public static void main(String[] args) { 
+	
+	//Initializing array 
+	int i, j; 
+	int[][] a = new int[3][3]; 
+	int[][] b = new int[3][3]; 
+	int[][] c = new int[3][3]; 
+	
+	//creating scanner class object 
+	Scanner s = new Scanner(System.in); 
+	
+	//input for 1st matrix 
+	System.out.print("Enter 9 elements for first matrix:\n "); 
+	for(i=0; i<3; i++) { 
+		for(j=0; j<3; j++) { 
+			a[i][j] = s.nextInt(); 
+		} 
+		} 
+	
+	//input for 2nd matrix 
+	System.out.print("Enter 9 elements for second matrix:\n "); 
+	for(i=0; i<3; i++) { 
+		for(j=0; j<3; j++) { 
+			b[i][j] = s.nextInt(); } } 
+	for(i=0; i<3; i++) { 
+		for(j=0; j<3; j++) { 
+			c[i][j] = a[i][j] - b[i][j]; 
+		} 
+		} 
+	
+	//Addition result 
+	System.out.println("\n----SubtractionS Result----"); 
+	for(i=0; i<3; i++) { 
+		for(j=0; j<3; j++) { 
+		System.out.print(c[i][j]+ " "); 
+		} 
+	System.out.print("\n"); } 
+	} 
+
+	}

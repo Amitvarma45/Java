37065e15c77040efc57dc0410cbd46bba105277a
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-25 10:23:03 +0530

    Add files via upload

---
+++ Practical/Collection/LinkList.java
@@ -0,0 +1,18 @@
+package Corejava;
+
+import java.util.*;  
+
+public class LinkList{  
+public static void main(String args[]){  
+LinkedList<String> al=new LinkedList<String>();  
+al.add("Ravi");  
+al.add("Vijay");  
+al.add("Ravi");  
+al.add("Ajay");  
+Iterator<String> itr=al.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}
+}
+}
+

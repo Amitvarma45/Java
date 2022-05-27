107b8d24680dc432820daa1e775925f3ad8e92de
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-27 11:46:49 +0530

    Add files via upload

---
+++ Practical/Java Beans/EmployeeOne.java
@@ -0,0 +1,17 @@
+package Corejava;
+
+public class EmployeeOne implements java.io.Serializable{ 
+ private int id; 
+ private String name; 
+ public EmployeeOne(){} 
+ public void setId(int id){this.id=id;} 
+ public int getId(){return id;} 
+ public void setName(String name){this.name=name;} 
+ public String getName(){return name;}
+
+ 
+	
+} 
+ 
+
+
---
+++ Practical/Java Beans/Test.java
@@ -0,0 +1,9 @@
+package Corejava;
+
+public class Test {
+	public static void main(String args[]){ 
+		 EmployeeOne e=new EmployeeOne();//object is created 
+		 e.setName("Arjun");//setting value to the object 
+		 System.out.println(e.getName()); 
+		 }
+}

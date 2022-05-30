7479c27f8926ca7ec6e3c6e50deed272354ebba3
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-05-30 11:41:07 +0530

    Add files via upload

---
+++ Lab Session/26-05-2022/ArrayDecreasingOrder.java
@@ -0,0 +1,40 @@
+//Program for Decreasing order of Array
+
+package Corejava;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+ class ArrayDecreasingOrder {
+
+	public static void main(String[] args) {
+		
+		//Creating len variable using int data type
+		int len; 
+		
+		//Creating object of Scanner class
+		Scanner sc = new Scanner(System.in);
+		  
+		//taking Input value from User
+		System.out.println("Enter Array length");
+		len=sc.nextInt();
+		
+		//Creating Integer array
+		Integer arr[]=new Integer[len];
+		System.out.println("Enter: "+len+" Elements To Store In Array\n");
+		
+		//Using for loop for taking values
+		    for(Integer i=0;i<len;i++) {
+		        arr[i]=sc.nextInt();
+		    }
+		    
+		    //Using collection for getting reverse order of array
+		    Arrays.sort(arr, Collections.reverseOrder()); 		
+			 
+		    //printing Output
+			System.out.printf("In descending Order Modified arr[] : \n%s\n\n",Arrays.toString(arr)); 
+			} 
+	}
+
+

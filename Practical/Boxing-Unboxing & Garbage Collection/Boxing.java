6244c965f97b0b62e84068b0491bfca9e8576144
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-06-06 11:22:45 +0530

    Add files via upload

---
+++ Practical/Boxing-Unboxing & Garbage Collection/Boxing.java
@@ -0,0 +1,20 @@
+package Corejava;
+
+public class Boxing {
+
+	public static void main(String[] args) {
+		// Assigning primitive long value to wrapper long variable x
+		Long x=430L;
+		// Assigning primitive char value to Wrapper Character variable y.
+		Character y='a';
+		int a = 50;
+		Integer a2 = new Integer(a);
+		Integer a3 = 5;//Boxing
+		System.out.println(a2+""+a3);
+		System.out.println(x);
+		System.out.println(y);
+		
+
+	}
+
+}

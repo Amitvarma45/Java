349dd96392738ab366719a5ce1b5790a47a66b2e
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-06-23 14:06:52 +0530

    Add files via upload

---
+++ Lab Session/23-06-2022/SnakeCase.java
@@ -0,0 +1,29 @@
+package Corejava;
+
+class SnakeCase
+{
+ public String camelToSnake(String str)
+ {
+	 //variable
+	 String stt="";
+
+//Assighnment Operator
+stt+=Character.toLowerCase(str.charAt(0));
+
+//for loop
+for(int i=1;i<str.length();i++)
+{
+ char ch=str.charAt(i);
+if (Character.isUnicodeIdentifierPart(ch))
+{
+stt=stt+'_';
+ stt=stt+Character.toLowerCase(ch);
+ }
+else
+{
+stt=stt+ch;
+ }  
+}
+return stt;
+ }
+}
---
+++ Lab Session/23-06-2022/SnakeRun.java
@@ -0,0 +1,11 @@
+package Corejava;
+
+class SnakeRun
+{
+public static void main(String args[])throws Exception{
+// creating a object of SnakeCase
+SnakeCase sn=new SnakeCase();
+//output
+System.out.println(sn.camelToSnake("DoSelect"));
+	}
+}

209774b708bb4a3ee48b5dc0ec79cc92e372812e
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-06-30 15:52:47 +0530

    Add files via upload

--- Lab Session/30-06-2022/Doctor.java
+++ Lab Session/30-06-2022/Doctor.java
@@ -66,28 +66,5 @@ class SpecialistDoctor extends Doctor
         System.out.println("Specialist: "+specialist);
     }
 }
- class Result {
-    public static void main(String[] args) {
-    	
-    	//object of Scanner class
-        Scanner sc = new Scanner(System.in);
-        String t = sc.nextLine();
+ 
 
-        //if else statements
-        if(t.equalsIgnoreCase("Specialist"))
-        {
-            SpecialistDoctor s = new SpecialistDoctor(Integer.parseInt(sc.nextLine()), sc.nextLine(), sc.nextLine(), sc.nextLine());
-            s.getDetails();
-        }
-        else if(t.equalsIgnoreCase("Doctor"))
-        {
-            Doctor s = new Doctor(Integer.parseInt(sc.nextLine()), sc.nextLine(), sc.nextLine());
-            s.getDetails();
-        }
-        else
-        {
-            System.out.println("Invalid Input");
-        }     
-        sc.close();  
-    }
-}

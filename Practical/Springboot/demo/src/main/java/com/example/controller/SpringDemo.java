f39c112c4f2fc2997d124fea96baf441c99fcfa6
Amit Varma <61784605+Amitvarma45@users.noreply.github.com>
2022-07-26 23:10:36 +0530

    Add files via upload

---
+++ Practical/Springboot/demo/src/main/java/com/example/controller/SpringDemo.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringDemo {
+	@GetMapping("/hi")
+	public String sayHi() {
+		
+		System.out.println("Hello !!");
+		return "Hello, Welcome to Spring Boot World";
+		
+	}
+}
